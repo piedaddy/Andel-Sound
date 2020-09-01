d971fea97cb0e1e17a8c78dc5709c1d48a68ac19
Gabrielle Wildfeuer <gpwildfeuer@email.wm.edu>
2020-09-01 21:45:28 +0200

    enabling build for netlify deployment

---
+++ build/precache-manifest.51ae3987790cf9c4402c798755c094c2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56da5952aa3f62fc7b2a4c3fa78d357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d852a9c511bade3c0d2",
+    "url": "/static/css/main.301d9349.chunk.css"
+  },
+  {
+    "revision": "c9a5facf94e971be4275",
+    "url": "/static/js/2.d08fb166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d08fb166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d852a9c511bade3c0d2",
+    "url": "/static/js/main.17a67325.chunk.js"
+  },
+  {
+    "revision": "6db29585e01fb34a6d8b",
+    "url": "/static/js/runtime-main.b4e4e81d.js"
+  },
+  {
+    "revision": "1e8d8e2a0bd7fdfa0073df180cea7dd3",
+    "url": "/static/media/computer_1.1e8d8e2a.JPG"
+  },
+  {
+    "revision": "fefbdaf3d8bd92d3d3abf88fd4711e82",
+    "url": "/static/media/computer_2.fefbdaf3.JPG"
+  },
+  {
+    "revision": "c71ec82ffd4fb59e681f9e748930137b",
+    "url": "/static/media/control_room_1.c71ec82f.JPG"
+  },
+  {
+    "revision": "22315e4f348e441d15029bd52d39ca91",
+    "url": "/static/media/control_room_3.22315e4f.JPG"
+  },
+  {
+    "revision": "8d4f72361ed16665dda326f40bd7195e",
+    "url": "/static/media/preamps_1.8d4f7236.JPG"
+  },
+  {
+    "revision": "e56c4b47560fdf69cedb45e42b254cdc",
+    "url": "/static/media/recording_room_1.e56c4b47.JPG"
+  },
+  {
+    "revision": "40c8d959482a1574d1c3f86f068541a5",
+    "url": "/static/media/recording_room_2.40c8d959.JPG"
+  },
+  {
+    "revision": "29adba5d65d5ecf02946d085610cee77",
+    "url": "/static/media/soundbooth_1.29adba5d.JPG"
+  }
+]);
