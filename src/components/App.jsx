5a6f9118f2d90a722794d3780553e1f03fc1c31d
Gabrielle Wildfeuer <gpwildfeuer@email.wm.edu>
2020-09-28 17:25:23 +0200

    added burger menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,20 @@ import Photos from "./Photos/Photos";
 import Gear from "./Gear/Gear";
 import Footer from "./Footer/Footer";
 import Contact from "./Contact/Contact";
-import References from './References/References';
+import References from "./References/References";
+import MenuSVG from "./Menu/MenuSVG";
 
 function App() {
   const [inEng, setInEng] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleLangChange = () => {
     setInEng(!inEng);
   };
+
+  function handleShowMenu() {
+    setShowMenu(!showMenu);
+  }
 
   return (
     <div className="App">
@@ -21,6 +27,9 @@ function App() {
         {/* <img src="./img/logo.jpeg" alt="Andel Sound Logo"/> */}
         {/* <button onClick={handleLangChange}> {inEng ? "EN" : "CZ"}</button> */}
         <nav className="nav">
+          <div className="nav-logo">
+            <img src={require("../photos/AS_2.PNG")} alt="Andel Sound Logo"/>
+          </div>
           <div className="lang">
             <form action="#">
               <div className="lang__range">
@@ -37,11 +46,32 @@ function App() {
               </div>
             </form>
           </div>
+          <div className="nav-burger">
+              {/* <img
+                src={require("../photos/menu.png")}
+                alt="menu icon"
+                onClick={handleShowMenu}
+              ></img> */}
+              <div className="menu-svg"  onClick={handleShowMenu}>
+              <MenuSVG />
+              </div>
+            {showMenu ? (
+              <div className="nav-burger__links">
+                <a href="#about" onClick={handleShowMenu} >{inEng ? "about" : "o nás"}</a>
+                <a href="#references" onClick={handleShowMenu}>{inEng ? "references" : "reference"}</a>
+                <a href="#photos" onClick={handleShowMenu}>{inEng ? "photos" : "fotky"}</a>
+                <a href="#gear" onClick={handleShowMenu}>{inEng ? "equiptment" : "vybavení"}</a>
+                <a href="#contact" onClick={handleShowMenu}>{inEng ? "contact" : "kontact"}</a>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
           <div className="nav-links">
             <a href="#about">{inEng ? "about" : "o nás"}</a>
             <a href="#references">{inEng ? "references" : "reference"}</a>
-            <a href="#gear">{inEng ? "equiptment" : "vybavení"}</a>
             <a href="#photos">{inEng ? "photos" : "fotky"}</a>
+            <a href="#gear">{inEng ? "equiptment" : "vybavení"}</a>
             <a href="#contact">{inEng ? "contact" : "kontact"}</a>
           </div>
           {/* <div className="nav-icons">
@@ -57,9 +87,9 @@ function App() {
         <References inEng={inEng} />
         <Gear inEng={inEng} />
         <Photos inEng={inEng} />
+
         <Contact inEng={inEng} />
         <Footer inEng={inEng} />
-
       </div>
     </div>
   );
